fd7bfc969c4af390b243ea4b793ee29e45c90447
rohit01 <hello@rohit.io>
2016-07-29 02:57:16 +0530

    rename

--- scripts/global.sh
+++ scripts/global.sh
@@ -7,4 +7,4 @@ set -e
 
 CONFIG_FILE="/etc/graylog/server/server.conf"
 INIT_CONFIG_FILE="/etc/default/graylog-server"
-ELASTICSEARCH_YML_FILE="/etc/graylog/server/graylog-elasticsearch.yml"
+ELASTICSEARCH_YML_FILE="/etc/graylog/server/elasticsearch.yml"
